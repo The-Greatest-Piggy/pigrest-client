325a27863374c6b1b235550f38dd050ba08b178a
yoonsun_jun <yoonsun_jun@tmax.co.kr>
2024-11-01 15:07:23 +0900

    feat: 홈화면 masonry layout 적용하여 뷰 구현
    FEAT
    - @egjs/react-infinitegrid 적용
    
    TODO
    - 라이브러리 제거, 직접 구현

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,26 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "media.istockphoto.com",
+        port: "",
+      },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        port: "",
+      },
+      {
+        protocol: "https",
+        hostname: "naver.github.io",
+        port: "",
+        pathname: "/egjs-infinitegrid/assets/image/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,68 @@
-export default function Home() {
+"use client";
+
+import { useState } from "react";
+import { MasonryInfiniteGrid } from "@egjs/react-infinitegrid";
+import Image from "next/image";
+
+export type TGetItems = {
+  nextGroupKey: number;
+  count: number;
+};
+
+function getItems({ nextGroupKey, count }: TGetItems) {
+  const nextItems = [];
+  const nextKey = nextGroupKey * count;
+
+  for (let i = 0; i < count; i++) {
+    nextItems.push({ groupKey: nextGroupKey, key: nextKey + i });
+  }
+  return nextItems;
+}
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const Item = ({ num }: any) => {
   return (
-    <div className="h-full grid items-center justify-items-center">hello?</div>
+    <div className="inline-block w-[280px]">
+      <div className="overflow-hidden rounded-xl">
+        <Image
+          src={`https://naver.github.io/egjs-infinitegrid/assets/image/${
+            (num % 33) + 1
+          }.jpg`}
+          alt="egjs"
+          width={500}
+          height={500}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default function Home() {
+  const [items, setItems] = useState(() =>
+    getItems({ nextGroupKey: 0, count: 10 })
+  );
+  return (
+    <div className="w-full">
+      <MasonryInfiniteGrid
+        align="justify"
+        gap={5}
+        onRequestAppend={(e) => {
+          const nextGroupKey = (+e.groupKey! || 0) + 1;
+
+          setItems([
+            ...items,
+            ...getItems({ nextGroupKey: nextGroupKey, count: 10 }),
+          ]);
+        }}
+      >
+        {items.map((item, idx) => (
+          <Item
+            data-grid-groupKey={item.groupKey}
+            key={`${item.key}-${idx}`}
+            num={item.key}
+          />
+        ))}
+      </MasonryInfiniteGrid>
+    </div>
   );
 }
