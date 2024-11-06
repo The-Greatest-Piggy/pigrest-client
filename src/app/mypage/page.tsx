34bb7e446f7e8bd539b8671cb4c47df3295d7e5c
yoonsun_jun <yoonsun_jun@tmax.co.kr>
2024-11-06 18:42:19 +0900

    [#3]feat: 마이페이지 뷰 구현중
    - 상단 유저 정보 뷰 구현
    - 하단 앨범 css 작업 진행 중

--- src/app/mypage/page.tsx
+++ src/app/mypage/page.tsx
@@ -1,3 +1,45 @@
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button";
+import { COLLECTION_DETAIL } from "@/constants/collections";
+import { MyCollections } from "../../components/custom/mypage/MyCollections";
+
 export default function MyPage() {
-  return <div>this is my page</div>;
+  const { user } = COLLECTION_DETAIL;
+  return (
+    <div className="flex flex-col items-center mt-12 space-y-24">
+      {/* 유저 정보 */}
+      <div className="flex flex-col items-center space-y-4">
+        {/* 프로필 사진 */}
+        <Avatar className="w-36 h-36">
+          <AvatarImage src="pigs/pig2.jpg" alt="profileImg" />
+          <AvatarFallback>P</AvatarFallback>
+        </Avatar>
+
+        {/* 유저 이름 */}
+        <p className="font-bold text-3xl">{user.name}</p>
+
+        {/* 계정명, 팔로워 수 */}
+        <div className="flex flex-col items-center">
+          <p className="text-font3 text-sm">@{user.accounts}</p>
+          {/* // TODO: 팔로잉, 팔로워 */}
+          <p className="font-medium text-md">
+            친구 <span>{user.friendsList.length}</span>명
+          </p>
+        </div>
+
+        {/* 공유, 프로필수정 */}
+        <div className="space-x-3">
+          <Button className="rounded-2xl bg-sub4 text-font1 text-base font-semibold hover:bg-sub3">
+            공유
+          </Button>
+          <Button className="rounded-2xl bg-sub4 text-font1 text-base font-semibold hover:bg-sub3">
+            프로필 수정
+          </Button>
+        </div>
+      </div>
+
+      {/* 컬렉션 */}
+      <MyCollections />
+    </div>
+  );
 }
--- src/typings/index.ts
+++ src/typings/index.ts
@@ -8,18 +8,21 @@ export type TMockData = {
 export type TCollectionDetail = {
   image: {
     imgurl: string; // 사진 경로
-    width: number; // 사진 너비
-    height: number; // 사진 높이
+    width?: number; // 사진 너비
+    height?: number; // 사진 높이
     title: string; // 제목
     description: string; // 설명
     collections: string[]; // 컬렉션
     tags: string[]; // 태그들
-    comments: string[]; // 댓글들
-    likes: number; // 좋아요 개수
+    comments?: string[]; // 댓글들
+    likes?: number; // 좋아요 개수
+    savedAt?: Date; // 저장된 날짜
   };
   user: {
     name: string; // 사진을 등록한 사람의 이름
     accounts: string; // 사진 등록한 사람의 아이디
-    // 팔로잉, 팔로워 관계
+    friendsList: string[]; // 친구 목록 (친구 요청, 친구 수락 필요)
   };
 };
+
+export type TMyPageCollections = TCollectionDetail[];
