9ebac511131cdd2b50531dc929c15ba131e830c4
yoonsunny17 <jyoonsun0217@gmail.com>
2025-02-16 21:48:01 +0900

    feat: 홈페이지 레이아웃 구현
    - css로 masonry layout 구현
    - image 개수가 부족할 경우, 오른쪽에 여백이 생기는 이슈 > 추후 masonry 라이브러리 사용해야 할지에 대한 결정 필요

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,37 @@
+interface TestProps {
+  height: string;
+  text: string;
+}
+
+const TestBox: React.FC<TestProps> = ({ height, text }) => {
+  return (
+    <div
+      className={`${height} bg-zinc-300 inline-block w-full break-inside-avoid mb-5 rounded-lg`}
+    >
+      {text}
+    </div>
+  );
+};
+
 export default function Home() {
-  return <div className="bg-red-50 h-full">hello world</div>;
+  return (
+    <div className="flex-1 columns-2 md:columns-3 lg:columns-4 xl:columns-5 2xl:columns-6 gap-5">
+      <TestBox height="h-[250px]" text="1" />
+      <TestBox height="h-[120px]" text="2" />
+      <TestBox height="h-[180px]" text="3" />
+      <TestBox height="h-[130px]" text="4" />
+      <TestBox height="h-[270px]" text="5" />
+      <TestBox height="h-[320px]" text="6" />
+      <TestBox height="h-[110px]" text="7" />
+      <TestBox height="h-[190px]" text="8" />
+      <TestBox height="h-[210px]" text="9" />
+      <TestBox height="h-[80px]" text="10" />
+      <TestBox height="h-[220px]" text="11" />
+      <TestBox height="h-[210px]" text="12" />
+      <TestBox height="h-[100px]" text="13" />
+      <TestBox height="h-[160px]" text="14" />
+      <TestBox height="h-[190px]" text="15" />
+      <TestBox height="h-[320px]" text="16" />
+    </div>
+  );
 }
