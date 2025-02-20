93fcff5f40dc09602e6a448195db1f19bdf46eeb
yoonsunny17 <jyoonsun0217@gmail.com>
2025-02-20 23:20:53 +0900

    [#6][#7]refactor: 프로필 수정하기 페이지 리팩토링
    FEAT
    - React-hook-form을 활용한 리팩토링 진행
    - username 유효성 검증 구현

--- src/app/profile/edit/page.tsx
+++ src/app/profile/edit/page.tsx
@@ -4,14 +4,34 @@ import Header from "@/components/common/Header";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 import React, { useRef, useState } from "react";
+import { useForm } from "react-hook-form";
+
+interface ProfileFormProps {
+  username: string;
+  bio: string;
+  profileImage: string;
+}
 
 const ProfileEdit = () => {
   const router = useRouter();
   const fileInput = useRef<HTMLInputElement>(null);
-  const [profileImage, setProfileImage] = useState(
-    "/images/default_avatar.png"
-  );
   const [imageFile, setImageFile] = useState<File | null>(null); // 서버로 보낼 파일 데이터
+
+  const {
+    register,
+    handleSubmit,
+    watch,
+    setValue,
+    formState: { errors },
+  } = useForm<ProfileFormProps>({
+    defaultValues: {
+      username: "", // TODO: 기존 설정되어 있는 username이 보여야 함
+      bio: "",
+      profileImage: "/images/default_avatar.png",
+    },
+  });
+
+  const profileImage = watch("profileImage");
 
   // 버튼 클릭 시, 인풋을 클릭한 효과를 준다
   const handleImageClick = () => {
@@ -23,29 +43,44 @@ const ProfileEdit = () => {
     if (files && files[0]) {
       const file = files[0];
       const url = URL.createObjectURL(file);
-      setProfileImage(url); // 화면에 보여줄 이미지
-      setImageFile(file); // 서버에 넘겨줄 파일
+      setValue("profileImage", url); // 화면에 보여줄 이미지
+      setImageFile(file); // 서버에 넘겨줄 File 객체 저장
     }
   };
 
   const resetImage = () => {
-    setProfileImage("/images/default_avatar.png");
+    setValue("profileImage", "/images/default_avatar.png");
     setImageFile(null);
     if (fileInput.current) {
       fileInput.current.value = "";
     }
   };
 
-  console.log(profileImage, imageFile);
+  const onSubmit = async (data: ProfileFormProps) => {
+    // FormData 생성
+    const formData = new FormData();
+
+    formData.append("username", data.username);
+    formData.append("bio", data.bio);
+
+    // 이미지 파일이 있는 경우 추가
+    if (imageFile) {
+      formData.append("profileImage", imageFile);
+    }
+
+    // TODO: api 호출 > formData 전송
+    router.back();
+  };
+
   return (
     <div className="space-y-9">
       <Header
         label="프로필 수정"
         showBackArrow
         btnLabel="수정하기"
-        btnOnClick={() => router.back()}
+        btnOnClick={handleSubmit(onSubmit)}
       />
-      <div className="flex flex-row gap-16 mx-5 max-w-screen-sm">
+      <div className="flex flex-row gap-16 mx-5 max-w-screen-md">
         <div className="flex-none flex flex-col gap-5 items-center">
           <Image
             src={profileImage}
@@ -70,27 +105,49 @@ const ProfileEdit = () => {
         <div className="flex flex-1 flex-col gap-8">
           {/* username */}
           <div className="flex flex-col gap-2">
-            <label className="text-xs font-medium" htmlFor="username">
+            <label className="text-sm font-medium" htmlFor="username">
               닉네임
             </label>
             <input
+              {...register("username", {
+                required: "닉네임을 입력해주세요.",
+                minLength: {
+                  value: 4,
+                  message: "닉네임은 4자 이상 12자 이하로 입력해주세요.",
+                },
+                maxLength: {
+                  value: 12,
+                  message: "닉네임은 4자 이상 12자 이하로 입력해주세요.",
+                },
+                pattern: {
+                  value: /^[a-z0-9가-힣ㄱ-ㅎㅏ-ㅣ_]+$/,
+                  message:
+                    "닉네임은 한글, 영문 소문자, 숫자, 특수문자(_)로만 구성해야 합니다.",
+                },
+              })}
               type="text"
               id="username"
               name="username"
-              placeholder="닉네임을 입력해 주세요."
+              placeholder="닉네임을 입력해주세요."
               className="block w-full border px-4 py-3 rounded-lg text-sm text-zinc-700"
             />
+            {errors.username && (
+              <span className="text-red-500 text-xs mt-1">
+                {errors.username.message}
+              </span>
+            )}
           </div>
           {/* bio */}
           <div className="flex flex-col gap-2">
-            <label className="text-xs font-medium" htmlFor="bio">
+            <label className="text-sm font-medium" htmlFor="bio">
               소개
             </label>
             <textarea
+              {...register("bio")}
               name="bio"
               id="bio"
               rows={3}
-              maxLength={150}
+              maxLength={100}
               placeholder="회원님에 대해 설명해 주세요."
               className="resize-none block w-full border px-4 py-3 rounded-lg text-sm text-zinc-700"
             />
