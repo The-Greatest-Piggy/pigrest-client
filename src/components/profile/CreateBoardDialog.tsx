5909e8b9c654bef221475c70b7052062bcef6dca
yoonsunny17 <jyoonsun0217@gmail.com>
2025-03-03 23:54:44 +0900

    [#18]feat: 보드 생성하기 모달 기능 추가
    FEAT
    - 보드 생성하기 모달 > 공개 여부 radio button 추가
    - Radio, RadioGroup 컴포넌트 분리

--- src/components/profile/CreateBoardDialog.tsx
+++ src/components/profile/CreateBoardDialog.tsx
@@ -1,4 +1,6 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 import Button from "../common/Button";
 import {
   Dialog,
@@ -8,8 +10,11 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
+import RadioGroup from "../common/Radio";
 
 const CreateBoardDialog = () => {
+  const [isPublic, setIsPublic] = useState<"public" | "private">("public"); // defualt는 전체공개(public)
+
   return (
     <Dialog>
       <DialogTrigger asChild>
@@ -23,17 +28,44 @@ const CreateBoardDialog = () => {
         <DialogHeader>
           <DialogTitle className="text-2xl">보드 만들기</DialogTitle>
         </DialogHeader>
-        <div className="flex flex-col">
+        <div className="flex flex-col gap-6">
+          {/* board name */}
           <div className="flex-1 space-y-2">
-            <label className="text-sm font-medium" htmlFor="new-board">
+            <label className="text-sm font-medium" htmlFor="new-board-name">
               이름
             </label>
             <input
               type="text"
-              id="new-board"
-              name="new-board"
+              id="new-board-name"
+              name="new-board-name"
               placeholder="보드 이름을 설정해 주세요."
               className="block w-full border px-4 py-3 rounded-lg text-md text-zinc-700"
+            />
+          </div>
+
+          {/* public/private radio btns */}
+          <div className="flex-1 space-y-2">
+            <label className="text-sm font-medium" htmlFor="new-board-ispublic">
+              공개 여부
+            </label>
+            {/* radio group */}
+            <RadioGroup
+              name="ispublic"
+              options={[
+                {
+                  id: "public",
+                  value: "public",
+                  label: "공개",
+                },
+                {
+                  id: "private",
+                  value: "private",
+                  label: "비공개",
+                },
+              ]}
+              selectedValue={isPublic}
+              onChange={(value) => setIsPublic(value as "public" | "private")}
+              className="flex flex-row gap-20"
             />
           </div>
         </div>
